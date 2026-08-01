5ef725a83998b3e7b7ab5f76e8e407f90a27b9b2
Carla Sofia Martins <carla.s.ofia@hotmail.com>
2026-08-01 23:45:30 +0100

    updated some data

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { 
-  getAuth, 
-  GoogleAuthProvider, 
-  signInWithPopup, 
-  signOut 
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
 } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDMr7_UXU1di-lRItAED6FIQacUyO_ruU8",
-  authDomain: "finances-9b3e1.firebaseapp.com",
-  projectId: "finances-9b3e1",
-  storageBucket: "finances-9b3e1.firebasestorage.app",
-  messagingSenderId: "591671311834",
-  appId: "1:591671311834:web:4088b26b6f06b3ef8194e3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Inicializa o Firebase
